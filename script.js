0fcf9ae838e29b818fd59e99406034611de70ae2
Victor Brossa <brossajoao@gmail.com>
2025-02-05 13:32:13 -0500

    creating expenses

---
+++ script.js
@@ -0,0 +1,63 @@
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+const expenseList = document.querySelector("ul");
+
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, "");
+
+  value = Number(value) / 100;
+
+  amount.value = formatCurrencyCAD(value);
+};
+
+function formatCurrencyCAD(value) {
+  value = value.toLocaleString("en", {
+    style: "currency",
+    currency: "CAD",
+  });
+
+  return value;
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+
+  addExpense(newExpense);
+};
+
+function addExpense(newExpense) {
+  try {
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    const expenseInfo = document.createElement("div");
+    expenseInfo.classList.add("expense-info");
+
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = newExpense.expense;
+
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = newExpense.category_name;
+
+    expenseInfo.append(expenseName, expenseCategory);
+    expenseItem.append(expenseIcon, expenseInfo);
+    expenseList.append(expenseItem);
+  } catch (error) {
+    alert("Not possible.");
+  }
+}
